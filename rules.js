0c97bf549c1be140fcf5e8e88590b5a2a433bfed
SUASTRA50 <65449969+SUASTRA50@users.noreply.github.com>
2026-08-20 19:43:33 +0700

    Update rules.js

--- rules.js
+++ rules.js
@@ -1,15 +1,16 @@
 const MANNING_RULES = {
-    credits: {
-        stayover: 1,
-        departure: 1.0,
-        dropped: 1.0,
-        pickup: 0.4,
-        turndown: 1
-    },
-    targetCreditPerRA: 13,
-    creditDW: 11,
-    creditTrainee: 5,
-    creditTD1: 30,
-    creditTD2: 0,
-    targetArrivalPerSupervisor: 17
+  credits: {
+    stayover: 1,
+    departure: 1,
+    dropped: 1,
+    pickup: 0.5
+  },
+  targetCreditPerRA: 13,
+  creditDW: 11,
+  creditTrainee: 5,
+  creditTD1: 35,
+  creditTD2: 0,
+  creditPMShift: 5,
+  targetArrivalPerSupervisor: 17,
+  stayoverRooms: 6
 };
